8ecb80e658f65c37a657fe70ba5aa94989d74037
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-16 17:14:37 +0200

    wip: getting back to oauth

--- config/ally.ts
+++ config/ally.ts
@@ -5,7 +5,7 @@ const allyConfig = defineConfig({
   github: services.github({
     clientId: env.get('OAUTH_GITHUB_CLIENT_ID'),
     clientSecret: env.get('OAUTH_GITHUB_CLIENT_SECRET'),
-    callbackUrl: `https://127.0.0.1:4443/api/v1/auth/github/callback`,
+    callbackUrl: `${env.get('APP_URL')}/api/v1/auth/github/callback`,
     scopes: ['user:email', 'read:user'],
   }),
   // google: services.google({
