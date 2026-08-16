a6cc2f7284b0cc7e99385aa4c9abca9d840b2bf1
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-16 17:14:37 +0200

    wip: getting back to oauth

--- start/routes.ts
+++ start/routes.ts
@@ -10,7 +10,6 @@
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 import { controllers } from '#generated/controllers'
-import User from '#models/user'
 
 router.get('/auth', () => {
   return { hello: 'world' }
@@ -25,47 +24,10 @@ router
         router.get('verify', [controllers.VerifyTokens, 'verify']).use(middleware.auth())
         router.get('users', [controllers.Users, 'index']).use(middleware.auth())
         router.delete('users', [controllers.Users, 'destroy']).use(middleware.auth())
-        router
-          .get('/github/redirect', ({ ally, request }) => {
-            const redirect = request.input('redirect')
-            const safeRedirect = redirect.startsWith('/') ? redirect : '/'
-
-            return ally.use('github').redirect((req) => {
-              req.param('redirect', safeRedirect)
-            })
-          })
-          .as('github.redirect')
-
-        router
-          .get('/github/callback', async ({ ally, request, response }) => {
-            const gh = ally.use('github')
-
-            if (!gh) {
-              return response.badRequest({ error: 'GitHub provider not configured' })
-            }
-            if (gh.accessDenied()) {
-              return { error: 'Access denied' }
-            }
-            if (gh.hasError()) {
-              return { error: 'Access denied 3' }
-            }
-            const githubUser = await gh.user()
-            const user = await User.firstOrCreate(
-              { email: githubUser.email },
-              {
-                email: githubUser.email,
-                fullName: githubUser.name,
-                password: crypto.randomUUID(),
-              }
-            )
-            const tmp_token = await User.accessTokens.create(user)
-            const redirect = request.input('redirect', '/')
-            const redirectUrl = new URL(redirect, 'https://localhost:4443')
-            redirectUrl.search = ''
-            redirectUrl.searchParams.set('token', encodeURIComponent(tmp_token.value!.release()))
-            return response.redirect().toPath(redirectUrl.href)
-          })
-          .as('github.callback')
+        router.get('/github/redirect', ({ ally, session }) => {
+          session.put('redirect.previousUrl', '/login')
+          return ally.use('github').redirect()
+        })
       })
       .as('auth')
       .prefix('auth')
